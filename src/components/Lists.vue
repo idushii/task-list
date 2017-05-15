faffc2ae02d856e5c1d1f4b7d41579f25022d21f
Nicolay Ilinov <Nicolay Ilinov>
2017-05-15 21:17:17 +0700

    Добавил манифест

--- src/components/Lists.vue
+++ src/components/Lists.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="row">
     <div class="col s12 m12">
-      <div class="card">
+      <div class="card pink lighten-4">
         <div class="card-content">
           <span class="card-title">Списки <div class="waves-effect waves-light btn right" @click="newList"><i class="material-icons">add</i></div></span>
         </div>
